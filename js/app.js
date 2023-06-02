0b1067fa61d0e3e054ef1263d136530bdfb14bd9
Gorodeckism <132038437+Gorodeckism@users.noreply.github.com>
2023-06-02 15:58:14 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-if('serviceWorker' in navigator){
-
-  navigator.serviceWorker.register('/Zingsniatrakeris/sw.js')
-
-  navigator.serviceWorker.register('https://gorodeckism.github.io/Zingsniatrakeris/js/app.js')
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/Zingsniatrakeris/sw.js')
     .then(reg => console.log('service worker registered', reg))
     .catch(err => console.log('service worker not registered', err));
+
+  navigator.serviceWorker
+    .register('/Zingsniatrakeris/js/app.js')
+    .then(reg => console.log('app.js registered', reg))
+    .catch(err => console.log('app.js not registered', err));
 }
