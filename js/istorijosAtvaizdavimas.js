dd2c37f5e8b71dca1ca759c3130a9ef785cdd865
Gorodeckism <132038437+Gorodeckism@users.noreply.github.com>
2023-06-02 17:44:06 +0300

    Update istorijosAtvaizdavimas.js

--- js/istorijosAtvaizdavimas.js
+++ js/istorijosAtvaizdavimas.js
@@ -1,11 +1,16 @@
 const savedList = document.getElementById('saved-list');
 const savedData = JSON.parse(localStorage.getItem('myData')) || [];
 
-// einame per išsaugotus duomenis ir sukuriame HTML elementus
 for (const data of savedData) {
   const li = document.createElement('span');
-  const distanceText = data.distance !== undefined ? `${data.distance} m atstumo, ` : '';
+  const distanceText = data.distance !== undefined ? `${data.distance} m atstumo` : '';
   const stepsText = data.steps !== undefined ? `${data.steps} žingsnių` : '';
-  li.innerHTML = `<ion-item><ion-label>${data.time} nuėjote ${distanceText}${stepsText}</ion-label></ion-item>`;
+  const content = `${data.time} nuėjote ${distanceText}`;
+  
+  if (distanceText && stepsText) {
+    content += `, ${stepsText}`;
+  }
+  
+  li.innerHTML = `<ion-item><ion-label>${content}</ion-label></ion-item>`;
   savedList.appendChild(li);
 }
