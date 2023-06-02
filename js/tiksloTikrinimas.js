cc8e6e2e635e091e0417b7924f2b1a162b108a59
Gorodeckism <132038437+Gorodeckism@users.noreply.github.com>
2023-06-02 16:32:21 +0300

    Update tiksloTikrinimas.js

--- js/tiksloTikrinimas.js
+++ js/tiksloTikrinimas.js
@@ -1,27 +1,27 @@
-if(localStorage.getItem("atstumoTikslas") != null){
-    document.querySelector('#atstumasInput').value = localStorage.getItem("atstumoTikslas");
-  }
+if (localStorage.getItem("atstumoTikslas") != null) {
+  document.querySelector('#atstumasInput').value = localStorage.getItem("atstumoTikslas");
+}
 
 function issaugotiAtstuma() {
-    let atstumas = document.getElementById("atstumasInput").value;
-    localStorage.setItem("atstumoTikslas", atstumas);
+  let atstumas = document.getElementById("atstumasInput").value;
+  localStorage.setItem("atstumoTikslas", atstumas);
 }
 
 function patikrintiTiksla(atstumas) {
-    let tikslas = localStorage.getItem("atstumoTikslas"); //tikslas
-    if (atstumas >= tikslas) {
-        rodytiPranesima("Tikslas pasiektas! Atstumas: " + atstumas + " m");
-    }
+  let tikslas = parseInt(localStorage.getItem("atstumoTikslas")); // Convert to number
+  if (atstumas === tikslas) { // Check for equality
+    rodytiPranesima("Tikslas pasiektas! Atstumas: " + atstumas + " m");
+  }
 }
 
 function rodytiPranesima(pranesimas) {
-    if (Notification.permission === "granted") {
+  if (Notification.permission === "granted") {
+    new Notification(pranesimas);
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
         new Notification(pranesimas);
-    } else if (Notification.permission !== "denied") {
-        Notification.requestPermission().then(function (permission) {
-            if (permission === "granted") {
-                new Notification(pranesimas);
-            }
-        });
-    }
+      }
+    });
+  }
 }
