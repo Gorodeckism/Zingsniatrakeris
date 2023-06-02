7ebe53e8e4f0f0b1bafe47ed9724e2f2d3201fc7
Gorodeckism <132038437+Gorodeckism@users.noreply.github.com>
2023-06-02 18:10:07 +0300

    Update istorijosIsaugojimas.js

--- js/istorijosIsaugojimas.js
+++ js/istorijosIsaugojimas.js
@@ -1,12 +1,12 @@
 const myButton = document.querySelector('#stopButton');
+const myDiv = document.querySelector('#myDiv');
 
 myButton.addEventListener('click', () => {
   const distance = document.querySelector('#distance').textContent;
-  const steps = parseInt(document.querySelector('#stepCount').textContent);
   const time = new Date().toLocaleString();
 
   let data = JSON.parse(localStorage.getItem('myData')) || [];
-  data.push({ distance, steps, time });
+  data.push({ distance, time });
 
   localStorage.setItem('myData', JSON.stringify(data));
 });
