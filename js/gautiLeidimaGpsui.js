6afb74dfaaa10da7f7e0225167c9c40ea0ca675b
Gorodeckism <132038437+Gorodeckism@users.noreply.github.com>
2023-06-07 09:13:10 +0300

    Update gautiLeidimaGpsui.js

--- js/gautiLeidimaGpsui.js
+++ js/gautiLeidimaGpsui.js
@@ -3,22 +3,19 @@ function requestGeolocationPermission() {
     if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                // čia kažką galime daryti su vartotojo vietove tačiau čia dabar nieko nereikia
+              
             },
             (error) => {
-                // Parodome pranešimą prašant leidimo naudoti geolokaciją
+                
                 const alert = document.createElement('ion-alert');
                 alert.header = 'Leidimas naudoti geolokaciją';
                 alert.message = 'Norint naudoti vietovės funkciją, reikia leisti svetainei naudoti geolokacijos funkciją. Ar leidžiate?';
                 alert.buttons = [
-                    // {
-                    // text: 'Atšaukti',
-                    // role: 'cancel'
-                    // },
+                   
                     {
                         text: 'Leisti',
                         handler: () => {
-                            // prašome geolokacijos funkcijos leidimo
+                            
                             requestGeolocationPermission();
                         }
                     }
@@ -37,14 +34,9 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     // Iškviečiama funkcija, kuri patikrina geolokacijos leidimą
     requestGeolocationPermission();
   } else {
-    // Jei naudojamas kompiuteris, išvesk alert su pranešimu
-    // window.addEventListener('load', function() {
-    //     const alertElement = document.querySelector('#my-alert');
-    //     alertElement.present();
-    //   });
+    
     alert("Sveiki. Jūs tikėtina naudojate kompiuterį, kuris gali neturėti tikslų vietos nustatymą. Dėl to aplikacija gali tinkamai neveikti jūsų įrenginyje. Rekomenduojame naudoti įrangą su GPS funkcija.");
       
-    //alert("Jūs įžengėte į svetainę su kompiuteriu!");
-    // Iškviečiama funkcija, kuri patikrina geolokacijos leidimą
+    
     requestGeolocationPermission();
   }
